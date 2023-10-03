d90e5cd6f2e558ac75094479000c47caf3f16965
davideagosti <114385033+davideagosti@users.noreply.github.com>
2023-10-03 17:03:46 +0200

    Create liste.py

---
+++ liste.py
@@ -0,0 +1,13 @@
+import os
+List1 = [21,9,18,2,10]
+print("Lista1 iniziale: ", List1)
+List1.reverse()
+print("Lista1 invertita", List1)
+
+List2 = [91,67,1212,11,76,333,78,87,56,45]
+print("Lista2 iniziale: ", List2)
+List2.reverse()
+print("Lista2 invertita", List2)
+
+indexvalue = List2[2:6]
+print("Lista2 parziale", indexvalue)
